db14a1d40d8b38bbd6ae347ebb5f0c1b1080b1f3
rafael <rafaelfreitas77@gmail.com>
2025-03-03 16:17:54 -0300

    Editando codigo 1° fase

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Solitacação dos  Dados 
+
+        System.out.println("Por favor , digite o numero da Agencia ");
+        int agencia = scanner.nextLine();
+
+        System.out.println("Por favor digite o número da conta: ");
+        int numeroConta = scanner.nextLine();
+        scanner.nextLine(); // Consumir a quebra de linha 
+
+        System.out.println("Por favor, digite o nome do Cliente : ");
+        String nomeClinte = scanner.nextLine();
+
+        System.out.println("Por favor , Digite o saldo inicial: ");
+        double saldo = scanner.nextDouble();
+
+        //Exibir mensagem 
+
+        
+
+
+
     }
 }
