4c6b01984821c850924090bfaef576a61258db0b
rafael <rafaelfreitas77@gmail.com>
2025-03-03 16:41:54 -0300

    editando codigo 3° commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -14,7 +14,7 @@ public class ContaTerminal {
 
         System.out.println("Por favor digite o número da conta: ");
         String  numeroConta = scanner.nextLine();
-        scanner.nextLine(); // Consumir a quebra de linha 
+       
 
         System.out.println("Por favor, digite o nome do Cliente : ");
         String nomeClinte = scanner.nextLine();
@@ -24,7 +24,7 @@ public class ContaTerminal {
 
         //Exibir mensagem 
 
-        String mensagem  = "Olá " + nomeClinte + ", obrigado por criar uma conta em nosso banco , sua é :" + agencia + ",Conta" + numeroConta + "e seu saldo " + saldo + "Ja esta disponivel para saque.";
+        String mensagem  = "Olá " + nomeClinte + ", obrigado por criar uma conta em nosso banco , sua Agencia é :" +  agencia + "e numero da conta : "+ numeroConta +"aldo atual:  " + saldo + "Ja esta disponivel para saque.";
 
         System.out.println(mensagem);
         scanner.close();
