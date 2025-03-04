489c5809d7cbd8834f7ba990c33a0a3ec45bc74b
rafael <rafaelfreitas77@gmail.com>
2025-03-04 10:27:41 -0300

    nota

--- ControleFluxo/NotaEscolar/src/App.java
+++ ControleFluxo/NotaEscolar/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double nota = 10;
+        double nota2 = 8;
+
+        if(nota2 < nota )
+        nota = nota -  nota2;
+
+        System.out.println(nota);
+        
     }
 }
