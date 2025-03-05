215cb01c556b363abd33802de66e13243541df96
rafael <rafaelfreitas77@gmail.com>
2025-03-04 22:42:40 -0300

    nota1

--- ControleFluxo/NotaEscolar/src/App.java
+++ ControleFluxo/NotaEscolar/src/App.java
@@ -1,11 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int nota = 8;
+        int nota = 10;
 
         if(nota >= 7)
          System.out.println("Aprovado");
+
+         else if (nota >= 5 && nota < 7 )
+         System.out.println("Prova Recuperação");
+
+         else if (nota >= 5 && nota < 7)
+         System.out.println("Prova Recuperação");
+
         
         else 
         System.out.println("Reprovado"); 
+        
     }
 }
+
