3b63a413d94d329b1d5419dde61043f8e3c1c894
rafael <rafaelfreitas77@gmail.com>
2025-03-04 23:26:02 -0300

    SistemaMedida

---
+++ ControleFluxo/NotaEscolar/src/SistemaMedida.java
@@ -0,0 +1,39 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+
+        String sigla = "G";
+
+        switch(sigla){
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("Medio");
+                break;
+            }
+            case "G":{
+                System.out.println("Grande");
+                break;
+            }
+
+            
+        }        /*
+        String  sigla = "M";
+        
+        if(sigla =="P")
+        System.out.println("Pequeno");
+        else if(sigla == "M")
+        System.out.println("Medio");
+        else if(sigla == "G")
+        System.out.println("Grande");
+        else
+        System.out.println("Indefinido");
+        
+        */
+
+
+    }
+        
+    
+}
