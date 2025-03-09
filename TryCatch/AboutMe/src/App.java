a03f0df97f8b4b83967192f1bf81d95b96011d32
rafael <rafaelfreitas77@gmail.com>
2025-03-09 12:37:18 -0300

    exceções - erros

--- TryCatch/AboutMe/src/App.java
+++ TryCatch/AboutMe/src/App.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // Entrada de dados pelo usuario...
+        System.out.println("Digite seu nome ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome ");
+        String Sobbrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        String idade= scanner.next();
+
+        System.out.println("Digite sua Altura ");
+        String altura = scanner.next();
+
+        // imprimindo os dados obtidos pelo usuario
+
+        System.out.println("\nOlá,  me chamo, " + nome.toUpperCase() + "");
+        System.out.println("Tenho " + idade + " Anos");
+        System.out.println("Minha altura é: " + altura + " cm");
+        scanner.close();
+
     }
 }
